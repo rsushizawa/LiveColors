dabfc3491ced8cc8652342af2f3d2861d253c7fa
rsushizawa <rodrigo.syu@puccampinas.edu.br>
2024-10-12 13:09:02 -0300

    LIV-12 Setup query parameters fetch rout

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ app.get('/default',(req,res)=>{
     res.sendFile(path.join(__dirname,'public/default/default.html'));
 })
 
+// Transforma os parametros query em JSON
+app.get('/data', (req, res) => {
+    // Extrai os parametros query
+    const queryData = req.query;
+
+    // Prepare JSON response
+    const jsonData = {
+        message: "Hello, this is your JSON data",
+        query: queryData
+    };
+
+    res.json(jsonData);
+});
+
 app.listen(5000, function(){
     console.log("servidor ok!");
 });
